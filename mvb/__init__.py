ee5d39f075d1f9be705b047f81b4148637c85c1a
Abdelkrim <ab.zitouni@esi-sba.dz>
2024-03-23 07:42:19 +0100

    Removed the mvb package

--- mvb/__init__.py
+++
@@ -1,7 +0,0 @@
-# __init__.py for mvb module
-from .rfc import RandomForestClassifier
-from .etc import ExtraTreesClassifier
-from .svm import SVMVotersClassifier
-from .abc import OurAdaBoostClassifier, BaseAdaBoostClassifier
-from .mce import MultiClassifierEnsemble
-
